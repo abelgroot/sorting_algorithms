c173018bd40e6de0416a03de2e6ec7b2b0a92e83
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-10-27 14:34:12 +0300

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,25 +6,25 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *temp, *bigger;
+	listint_t *current, *temp, *bigger;
 
-    if(list == NULL || *list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 	{
-    	return;
+		return;
 	}
 	current = (*list)->next;
-	while(current != NULL)
+	while (current != NULL)
 	{
 		temp = current;
 		current = current->next;
-		while(temp->prev != NULL && temp->n < temp->prev->n)
+		while (temp->prev != NULL && temp->n < temp->prev->n)
 		{
 			bigger = temp->prev;
-			if(bigger->prev != NULL)
+			if (bigger->prev != NULL)
 				bigger->prev->next = temp;
 			else
 				(*list) = temp;
-			if(temp->next != NULL)
+			if (temp->next != NULL)
 				temp->next->prev = bigger;
 			temp->prev = bigger->prev;
 			bigger->next = temp->next;
