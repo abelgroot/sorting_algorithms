54b9f850503241219468f9a5e6f1458dc3b15fba
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-10-27 14:27:15 +0300

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,33 +1,36 @@
 #include "sort.h"
 /**
- * insertion_sort_list - a function to sort a duobly linked list of integers
+ * insertion_sort_list - a function to sort a doubly linked list of integers
  * using insertion sort
  * @list: a pointer to the head of a doubly linked list
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *temp;
+    listint_t *current, *temp, *bigger;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
+    if(list == NULL || *list == NULL || (*list)->next == NULL)
+	{
+    	return;
+	}
 	current = (*list)->next;
-	while (current != NULL)
+	while(current != NULL)
 	{
 		temp = current;
 		current = current->next;
-
-		while (temp->prev != NULL && temp->n < temp->prev->n)
+		while(temp->prev != NULL && temp->n < temp->prev->n)
 		{
-			temp->prev->next = temp->next;
-			if (temp->next != NULL)
-				temp->next->prev = temp->prev;
-			temp->next = temp->prev;
-			temp->prev = temp->next->prev;
-			if (temp->prev != NULL)
-				temp->prev->next = temp;
+			bigger = temp->prev;
+			if(bigger->prev != NULL)
+				bigger->prev->next = temp;
 			else
 				(*list) = temp;
-			temp->next->prev = temp;
+			if(temp->next != NULL)
+				temp->next->prev = bigger;
+			temp->prev = bigger->prev;
+			bigger->next = temp->next;
+			temp->next = bigger;
+			bigger->prev = temp;
+			print_list(*list);
 		}
 	}
 }
